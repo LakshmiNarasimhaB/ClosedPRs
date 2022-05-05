b9f3012aad5b07bcf98b6cc820e67b022ff05969
Lakshmi Narasimha B <lakshminarasimhabandaru@gmail.com>
2022-05-05 18:06:28 +0530

    Use Kotlin Dss instead of Groovy (#1)
    Use kotlin dss instead of groovy. Adds support for auto complete and click support for the dependencies references.

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    id(BuildPlugins.androidApplication)
+    id(BuildPlugins.kotlinAndroid)
+    id(BuildPlugins.kotlinAndroidExtensions)
+    id(BuildPlugins.kotlinKapt)
+}
+
+android {
+
+    compileSdkVersion = Sdk.compileVersion
+
+    defaultConfig {
+        minSdk = Sdk.minVersion
+        targetSdk = Sdk.targetVersion
+
+        applicationId = App.id
+        versionCode = App.versionCode
+        versionName = App.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        buildConfigField(
+            "String",
+            "API_KEY",
+            gradleLocalProperties(rootDir).getProperty("GIT_HUB_API_KEY")
+        )
+    }
+
+    buildTypes {
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+    }
+
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // Coroutines
+    implementation(Libraries.coroutinesCore)
+    implementation(Libraries.coroutinesAndroid)
+    // UI and Appcompat
+    implementation(Libraries.appcompat)
+    implementation(Libraries.constraintlayout)
+    implementation(Libraries.recyclerview)
+    implementation(Libraries.material)
+    // ViewModel and LiveData
+    implementation(Libraries.viewmodelKtx)
+    implementation(Libraries.livedataKtx)
+    implementation(Libraries.lifecycleRuntimeKtx)
+
+    // Retrofit
+    implementation(Libraries.retrofit)
+    implementation(Libraries.retrofitConverterGson)
+    implementation(Libraries.loggingInterceptor)
+
+    // Glide
+    implementation(Libraries.glide)
+    kapt(Libraries.glideCompiler)
+
+    // Unit Tests
+    testImplementation(TestLibs.junit)
+    testImplementation(TestLibs.mockk)
+    testImplementation(TestLibs.kotestAssertions)
+    testImplementation(TestLibs.androidxTest)
+    testImplementation(TestLibs.archTest)
+    testImplementation(TestLibs.coroutinesTest)
+}
